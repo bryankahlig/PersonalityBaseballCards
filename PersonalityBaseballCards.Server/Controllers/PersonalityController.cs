3c86ef3f144466d4730ca8e1c5b199832a19197f
Bryan Kahlig <bryan.kahlig@gmail.com>
2024-05-05 13:02:12 -0500

    Fix project references after solution refactor.

--- PersonalityBaseballCards.Server/Controllers/PersonalityController.cs
+++ PersonalityBaseballCards.Server/Controllers/PersonalityController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using PersonalityBaseballCards.Server.Models;
+using PersonalityBaseballCards.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
