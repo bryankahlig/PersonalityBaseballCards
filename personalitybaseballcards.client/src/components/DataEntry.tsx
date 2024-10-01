1973cb632a4bda53f669d0c2585eb94d26a26f5a
Bryan Kahlig <bryan.kahlig@gmail.com>
2024-09-30 21:57:14 -0500

    Fixing table formats and layout of data entry

--- personalitybaseballcards.client/src/components/DataEntry.tsx
+++ personalitybaseballcards.client/src/components/DataEntry.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
 export default class DataEntry extends React.Component {
+    constructor(defaultValue: {}) {
+        super(defaultValue);
+    }
     render() {
         return (
             <div className="dataentry-container">
                 <form>
-                    <div ><label>Name:</label>
-                        <div><input type="text" value="John Doe" id="name"/></div></div>
-                    <div ><label>Personality:</label>
-                        <div ><input type="text" value="INTJ" /></div></div>
-                    <div ><label>Love Language:</label>
-                        <div><input type="text" value="Gifts" /></div></div>
+                    <div className="flex-container">
+                        <div className="flex-item"><label>Name:</label>
+                            <div><input type="text" defaultValue="John Doe" id="name" /></div></div>
+                        <div className="flex-item"><label>Personality:</label>
+                            <div ><input type="text" defaultValue="INTJ" /></div></div>
+                        <div className="flex-item"><label>Love Language:</label>
+                            <div><input type="text" defaultValue="Gifts" /></div></div>
+                    </div>
                     <button type="submit">Set</button>
                 </form>
             </div>
         );
     }
+
 }
